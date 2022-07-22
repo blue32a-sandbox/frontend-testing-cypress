718c36e0aae26a3e63b3246257fae4c1c9776a12
blue32a <blue32a.jp@gmail.com>
2022-07-22 17:35:58 +0900

    viewport command

---
+++ cypress/e2e/commands/viewport.cy.js
@@ -0,0 +1,47 @@
+describe('viewportコマンドのテスト', () => {
+  const URL = Cypress.env('SITE_URL') + '/commands/viewport';
+
+  describe('iphone-x', () => {
+    beforeEach(() => {
+      cy.viewport('iphone-x');
+      cy.visit(URL);
+    });
+    it('.spは見えて、.pcは見えない', () => {
+      cy.get('.sp').should('be.visible');
+      cy.get('.pc').should('not.be.visible');
+    });
+  });
+
+  describe('ipad', () => {
+    beforeEach(() => {
+      cy.viewport('ipad-2');
+      cy.visit(URL);
+    });
+    it('.spは見えて、.pcは見えない', () => {
+      cy.get('.sp').should('be.visible');
+      cy.get('.pc').should('not.be.visible');
+    });
+  });
+
+  describe('ipad landscape', () => {
+    beforeEach(() => {
+      cy.viewport('ipad-2', 'landscape');
+      cy.visit(URL);
+    });
+    it('.pcは見えて、.spは見えない', () => {
+      cy.get('.pc').should('be.visible');
+      cy.get('.sp').should('not.be.visible');
+    });
+  });
+
+  describe('PC', () => {
+    beforeEach(() => {
+      cy.viewport(1024, 800);
+      cy.visit(URL);
+    });
+    it('.pcは見えて、.spは見えない', () => {
+      cy.get('.pc').should('be.visible');
+      cy.get('.sp').should('not.be.visible');
+    });
+  });
+});
