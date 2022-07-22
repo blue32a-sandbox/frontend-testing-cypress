1e1381bb784865659433789435f445763a146537
blue32a <blue32a.jp@gmail.com>
2022-07-22 18:39:56 +0900

    url command

---
+++ cypress/e2e/commands/url.cy.js
@@ -0,0 +1,21 @@
+describe('urlコマンドのテスト', () => {
+  beforeEach(() => {
+    cy.visit(Cypress.env('SITE_URL') + '/commands/url');
+  });
+
+  describe('No Args', () => {
+    it('URLの一部を検証する', () => {
+      cy.url().should('include', '/commands/url/');
+    });
+    it('URLの完全一致を検証する', () => {
+      cy.url().should('eq', 'http://127.0.0.1:5500/commands/url/');
+    });
+  });
+
+  describe('Option', () => {
+    it('URLをデコードして検証する', () => {
+      cy.get('a').click();
+      cy.url({ decode: true }).should('include', '#あいうえお');
+    });
+  });
+})
