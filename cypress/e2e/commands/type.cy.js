c2ada3acea2a0fbdb0513059637ac2f08d9e35f3
blue32a <blue32a.jp@gmail.com>
2022-07-25 14:29:22 +0900

    type command

---
+++ cypress/e2e/commands/type.cy.js
@@ -0,0 +1,46 @@
+describe('typeコマンドのテスト', () => {
+  beforeEach(() => {
+    cy.visit(Cypress.env('SITE_URL') + '/commands/type');
+  });
+
+  describe('Input', () => {
+    it('inputに入力する', () => {
+      cy.get('input#text')
+        .type('Hello, World');
+      cy.get('input#text')
+        .should('have.value', 'Hello, World');
+    });
+    it('textareaに入力する', () => {
+      cy.get('textarea#textarea')
+        .type('Hello,{enter}World');
+      cy.get('textarea#textarea')
+        .should('have.value', "Hello,\nWorld");
+    });
+    it('datalistから選択する', () => {
+      cy.get('input#datalist')
+        .type('Banana');
+      cy.get('input#datalist')
+        .should('have.value', 'Banana');
+    });
+    it('非入力要素または非テキストエリア要素に入力する', () => {
+      cy.get('#el')
+        .type('supercalifragilisticexpialidocious');
+    });
+    it('有効な日付を入力する', () => {
+      cy.get('input#date')
+        .type('2022-07-25');
+    });
+    it('有効な月を入力する', () => {
+      cy.get('input#month')
+        .type('2022-07');
+    });
+    it('有効な週を入力する', () => {
+      cy.get('input#week')
+        .type('2022-W07');
+    });
+    it('有効な時間を入力する', () => {
+      cy.get('input#time')
+        .type('23:02:15');
+    });
+  });
+});
