a811a732f5ac9cb45392f79a45fc40618d1ef8a1
blue32a <blue32a.jp@gmail.com>
2022-07-26 21:24:50 +0900

    intercept command

---
+++ cypress/e2e/commands/intercept.cy.js
@@ -0,0 +1,20 @@
+describe('interceptコマンドのテスト', () => {
+  beforeEach(() => {
+    cy.visit(Cypress.env('SITE_URL') + '/commands/intercept');
+  });
+
+  describe('Waiting on a request', () => {
+    it('GETリクエストが完了したあと、ステータスがGET doneになる', () => {
+      cy.intercept('GET', 'https://httpbin.org/delay/*').as('dilay');
+      cy.wait('@dilay');
+      cy.get('#status_get')
+        .should('have.text', 'GET done');
+    });
+    it('POSTリクエストが完了したあと、ステータスがPOST doneになる', () => {
+      cy.intercept('POST', 'https://httpbin.org/delay/*').as('dilay');
+      cy.wait('@dilay');
+      cy.get('#status_post')
+        .should('have.text', 'POST done');
+    });
+  });
+});
